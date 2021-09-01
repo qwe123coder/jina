6b030139997a22615c39b867370913013084ea09
Florian Hönicke <hoenicke.florian@gmail.com>
2021-09-01 17:26:55 +0200

    feat: pairing

--- jina/peapods/pods/k8slib/kubernetes_deployment.py
+++ jina/peapods/pods/k8slib/kubernetes_deployment.py
@@ -130,7 +130,7 @@ def get_cli_params(arguments: Namespace, skip_list: Tuple[str] = ()) -> str:
         'dynamic_routing',
         'hosts_in_connect',
         'polling_type',
-        'k8s_namespace',
+        # 'k8s_namespace',
         'uses_after',
         'uses_before',
         'replicas',
